7c61a5e3d8d928a811636865caea7aeee59f9abd
Alexandr <speedo210@gmail.com>
2021-06-06 22:29:32 +0300

    new lesson - Route and Path. add Profile and Dialogs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className={s.header}>
       <div className={s.box_img}>
-        <img src="https://image.flaticon.com/icons/png/512/809/809154.png" />
+        <img src="https://image.flaticon.com/icons/png/512/809/809154.png"/>
         <div className={s.box_text}>
           <span className={s.first_b_logo}>B</span>
           <p className={s.secant_b_logo}>booK</p>
